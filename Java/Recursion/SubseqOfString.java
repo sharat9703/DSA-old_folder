5a1b47f808e7db196e4655c9f13a2ff3d31219ea
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 <sharath.yaragatti@yahoo.com>
2023-03-21 17:40:20 +0530

    find all subsequences of a string

--- Java/Recursion/SubseqOfString.java
+++ Java/Recursion/SubseqOfString.java
@@ -1,14 +1,20 @@
 public class SubseqOfString {
     public static void subseq(String str,int idx,String newStr) {
         if(idx==str.length()){
-            System.out.println(newStr);
+            System.out.print(newStr+" ");
+            return;
         }
         char currChar = str.charAt(idx);
-        //
+        //to be 
+        subseq(str, idx+1, newStr+currChar);
+
+        //not to be
+        subseq(str, idx+1, newStr);
 
     }
     public static void main(String[] args) {
-     String str= "";   
+     String str= "abc";
+     subseq(str, 0, "");   
     }
     
 }
